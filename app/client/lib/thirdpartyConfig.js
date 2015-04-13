9cc6a4d9ce79890a60c4e8cb65d221f7146b0285
Nick Dodson <silentcicero@outlook.com>
2015-04-13 19:17:48 -0400

    Update thirdpartyConfig.js

--- app/client/lib/thirdpartyConfig.js
+++ app/client/lib/thirdpartyConfig.js
@@ -1,2 +1,2 @@
 // set providor
-web3.setProvider(new web3.providers.HttpProvider("http://172.16.170.136:8545/")); //8545 8080 10.10.42.116
+web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545/")); //8545 8080 10.10.42.116
