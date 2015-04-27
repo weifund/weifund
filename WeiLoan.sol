83f23c9ad0b76c141cf0a8681a219e8fb8229608
terzim <ma.terzi@tiscali.it>
2015-04-28 00:00:40 +0100

    Update WeiLoan.sol

--- WeiLoan.sol
+++ WeiLoan.sol
@@ -11,8 +11,8 @@ contract WeiLoan
 {
     struct User
     {
-        uint numCampaigns;
-        mapping(uint => uint) campaigns;
+        uint numLoans;
+        mapping(uint => uint) loans;
     }
     
     struct Funder 
@@ -35,92 +35,126 @@ contract WeiLoan
         uint numFunders;
         uint interest_rate;
         uint grace_period;
+        uint tenor;
         mapping (uint => Funder) funders;
         mapping (address => uint) toFunder;
     }
   
-    uint public numCampaigns;
-    mapping (uint => Campaign) public campaigns;
+    uint public numLoans;
+    mapping (uint => Loan) public loans;
     mapping (address => User) public users;
     
-    event onNewCampaign(address indexed _from, uint indexed _cid);
+    event onNewLoan(address indexed _from, uint indexed _cid);
     event onContribute(address indexed _from, uint indexed _cid, uint _value);
     event onPayout(address indexed _from, uint indexed _cid, uint _value);
     event onRefund(address indexed _from, uint indexed _cid, uint _value);
     
     modifier hasValue { if(msg.value > 0) _ }
     
-    function newCampaign(bytes32 _name, bytes32 _website
-    , address _beneficiary, uint _goal, uint _timelimit, uint _category)
+    function newLoan(bytes32 _name, bytes32 _website
+    , address _beneficiary, uint _goal, uint _timelimit, uint _category, uint _interest_rate, uint _grace_period, uint _tenor)
     {
         if(_goal > 0 && _timelimit > now){
-            uint cid = numCampaigns++; // campaignID is return variable
-            Campaign c = campaigns[cid];  // assigns reference
-            c.name = _name;
-            c.owner = msg.sender;
-            c.website = _website;
-            c.beneficiary = _beneficiary;
-            c.fundingGoal = _goal;
-            c.timelimit = _timelimit;
-            c.category = _category;
+            uint lid = numLoans++; // campaignID is return variable
+            Loan l = loans[lid];  // assigns reference
+            l.name = _name;
+            l.owner = msg.sender;
+            l.website = _website;
+            l.beneficiary = _beneficiary;
+            l.fundingGoal = _goal;
+            l.timelimit = _timelimit;
+            l.category = _category;
+            l.interest_rate = _interest_rate;
+            l.grace_period = _grace_period, 
+            l.tenor = _tenor;
             
             User u = users[msg.sender];
-            uint u_cid = u.numCampaigns++;
-            u.campaigns[u_cid] = cid;
+            uint u_lid = u.numLoans++;
+            u.loans[u_lid] = lid;
             
-            onNewCampaign(msg.sender, cid);
+            onNewLoan(msg.sender, lid);
         }
     }
     
-    function contribute(uint _cid) hasValue
+    function contribute(uint _lid) hasValue
     {
-        Campaign c = campaigns[_cid]; // Cannot be expired.
-        if(c.timelimit >= block.timestamp){
-            uint fid = c.numFunders++;
-            Funder f = c.funders[fid];
+        Loan l = loans[_lid]; // Cannot be expired.
+        if(l.timelimit >= block.timestamp){
+            uint fid = l.numFunders++;
+            Funder f = l.funders[fid];
             f.addr = msg.sender;
             f.amount = msg.value;
-            c.amount += f.amount;
-            c.toFunder[msg.sender] = fid;
-            onContribute(msg.sender, _cid, c.amount);
+            l.amount += f.amount;
+            l.toFunder[msg.sender] = fid;
+            onContribute(msg.sender, _lid, l.amount);
         }
     }
     
-    function refund(uint _cid)
+    function refund(uint _lid)
     {
-        Campaign c = campaigns[_cid];
-        if (block.timestamp > c.timelimit 
-        && c.amount < c.fundingGoal && c.amount > 0){
-            Funder f = c.funders[c.toFunder[msg.sender]];
+        Loan l = loans[_lid];
+        if (block.timestamp > l.timelimit 
+        && l.amount < l.fundingGoal && l.amount > 0){
+            Funder f = l.funders[l.toFunder[msg.sender]];
             if(f.amount > 0){
                 f.addr.send(f.amount);
-                c.amount -= f.amount;
-                onRefund(msg.sender, _cid, f.amount);
+                l.amount -= f.amount;
+                onRefund(msg.sender, _lid, f.amount);
                 f.amount = 0;
             }
         }
     }
   
-    function payout(uint _cid)
+    function payout(uint _lid)
     {
-        Campaign c = campaigns[_cid];
-        if (c.amount >= c.fundingGoal){
-            c.beneficiary.send(c.amount);
-            onPayout(msg.sender, _cid, c.amount);
-            c.amount = 0;
-            c.status = 1;
+        Loan l = loans[_lid];
+        if (l.amount >= l.fundingGoal){
+            l.beneficiary.send(l.amount);
+            onPayout(msg.sender, _lid, l.amount);
+            l.amount = 0;
+            l.status = 1;
         }
     }
     
-    function userCampaigns(address _addr, uint _u_cid) returns (uint cid)
+    function userLoans(address _addr, uint _u_lid) returns (uint lid)
     {
         User u = users[_addr];
-        cid = u.campaigns[_u_cid];
+        lid = u.loans[_u_lid];
     }
     
-    function campaignFunders(uint _cid, uint _f_id) returns (Funder f)
+    function campaignFunders(uint _lid, uint _f_id) returns (Funder f)
     {
-        Campaign c = campaigns[_cid];
-        f = c.funders[_f_id];
+        Loan l = loans[_lid];
+        f = l.funders[_f_id];
     }
+    
+    //masssi part
+    
+    //write a function that if time is above grace period starts the repayments of the loan
+    //the loan will be assumed to be a constant instalment loan so that each instalment payment would be something like
+    
+    /first converts annual interest rate to monthly
+    
+    interest_rate_m = interest_rate / 12;
+    
+    //then converts annual tenor into monthly tenor
+    
+    tenor_m = tenor * 12;
+    
+    //calculates the instalment amount
+    
+    l.instalment = l.amount * ((interest_rate_m*((1+interest_rate_m)^(tenor_m)))/(((1+interest_rate_m)^(tenor_m))-1))
+    
+    //calculates the percentage of funds put by each funder
+    
+    proportion of funder[fid] = sum contributed by funder[fid] / original loan amount 
+    
+    //pays out to each funder the proportion of its instalment
+    
+    send to funder[fid] = proportion of funder[fid] * loan instalments
+    
+    //residual amount
+    
+    new loan balance = previous loan balance - (instalment - interest payment for that period)
+    
 }
