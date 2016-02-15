d35d6a8ff8ff49f6670cf5b9a212642f563e2dd4
Nick Dodson <silentcicero@outlook.com>
2016-02-15 10:52:03 -0500

    Update WeiFund_Beta_Launch.sol

--- contracts/WeiFund_Beta_Launch.sol
+++ contracts/WeiFund_Beta_Launch.sol
@@ -1,7 +1,42 @@
-// WeiFund v1.0
-// Start, donate, payout and refund crowdfunding campaigns on Ethereum
-// If campaign goal is not reached by the stated expiry, all funds are refundable back to oringial contributor accounts
-// If campaign goal is reached or surpassed by stated expiry, all raised funds will be paid out to campaign beneficiary
+/*
+WeiFund v1.0
+
+This contract creates a crowdfunding platform. Start, donate to, payout and 
+refund crowdfunding campaigns on Ethereum.
+
+If the campaign goal is reached or surpassed by stated expiry, all raised 
+funds will be paid out to the campaign's beneficiary.
+
+If the campaign goal is not reached by the stated expiry, all funds are 
+refundable back to oringial contributors.
+
+Campaigns may also select a configuration contract which can be used for
+customized outward extensibility of campaigns to contracts like token or 
+registry systems.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+*/
 
 /// @title The core WeiFund confirgutation hook interface
 /// @author Nick Dodson <thenickdodson@gmail.com>
