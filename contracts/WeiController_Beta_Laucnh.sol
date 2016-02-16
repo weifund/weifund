8ffdea6113c39debc3f60332f892d09acf288a37
Nick Dodson <silentcicero@outlook.com>
2016-02-16 11:45:21 -0500

    Create WeiController_Beta_Laucnh.sol

---
+++ contracts/WeiController_Beta_Laucnh.sol
@@ -0,0 +1,150 @@
+/*
+WeiFund v1.0
+
+This contract creates a WeiFund token dispersal controller.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+*/
+
+/// @title The core WeiFund configuration hook interface
+/// @author Nick Dodson <thenickdodson@gmail.com>
+/// @dev This contract enables campaigns to interact with other contracts, such as equity dispersal mechanisms (controllers) and registries
+contract WeiFundConfig {
+    /// @notice Called when a new campaign has been created
+    /// @dev If a campaign specifies a configuration contract, this will be called when the new campaign is created 
+    /// @param _campaignID (campaign id) the campaign id
+    /// @param _owner (campaign owner) the campaign owner or creator
+    /// @param _fundingGoal (funding goal) the campaign funding goal
+    function newCampaign(uint _campaignID, address _owner, uint _fundingGoal) {}
+    
+    /// @notice Called when a new contribution has been made
+    /// @dev This will be called when a new contribution has been made to a campaign, this can be used for token generation
+    /// @param _campaignID (campaign id) the campaign id 
+    /// @param _contributor (contributor) the account that initially made the campaign contribution
+    /// @param _beneficiary (contribution beneficiary) the contribution beneficiary
+    /// @param _amountContributed (amount contributed) the amount contributed by the contributor
+    function contribute(uint _campaignID, address _contributor, address _beneficiary, uint _amountContributed) {}
+    
+    /// @notice Called when a new refund has been ordered
+    /// @dev This will be called when a campaign has failed and a contributor is ordering a refund of their contributed ether
+    /// @param _campaignID (campaign id) the campaign id 
+    /// @param _contributor (contributor) the campaign contributor address
+    /// @param _amountRefunded the amount refunded to the contributor
+    function refund(uint _campaignID, address _contributor, uint _amountRefunded) {}
+    
+    /// @notice Called when a campaign is being paid out
+    /// @dev This will be called when a campaign has succeceed and the funds are being paid out to the contributor
+    /// @param _campaignID (campaign id) the campaign id 
+    /// @param _amountPaid The amount paid out to the campaign beneficiary
+    function payout(uint _campaignID, uint _amountPaid) {}
+}
+
+contract Token {
+    /// @return total amount of tokens
+    function totalSupply() constant returns (uint256 supply) {}
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+/// @title The core WeiFund Controller/Token Dispersal Contract
+/// @author Nick Dodson <thenickdodson@gmail.com>
+contract WeiController is WeiFundConfig {
+    address token;
+    address weifund;
+    uint ratio;
+    address owner;
+    uint campaignID;
+    bool paidout;
+    
+    function WeiController_custom (address _weifund, address _token, uint _ratio) {
+        token = _token;
+        weifund = _weifund;
+        ratio = _ratio;
+        owner = msg.sender;
+    }
+    
+    function newCampaign (uint _campaignID, address _owner, uint _fundingGoal) {
+        if(_owner != owner || msg.sender != weifund)
+            return;
+            
+        campaignID = _campaignID;
+    }
+    
+    function contribute (uint _campaignID, address _contributor, address _beneficiary, uint _amountContributed) {
+        if(campaignID != _campaignID
+            || _amountContributed <= 0
+            || msg.sender != weifund
+            || paidout)
+            return;
+            
+        address receiver = _beneficiary;
+        uint tokenAmount = _amountContributed / ratio;
+        
+        if(receiver == address(0))
+            receiver = _contributor;
+            
+        Token(token).transfer(receiver, tokenAmount);
+    }
+    
+    function refund (uint _campaignID, address _contributor, uint _amountRefunded) {
+        if(campaignID != _campaignID || msg.sender != weifund)
+            return;
+    }
+    
+    function payout (uint _campaignID, uint _amountPaid) {
+        if(campaignID != _campaignID || msg.sender != weifund)
+            return;
+            
+        paidout = true;
+    }
+}
